edb68459ba019cdb4b577bfcbddf53842eaa2697
Hasibul Kobir <46620128+hasibulkabir@users.noreply.github.com>
2019-07-03 12:50:32 +0600

    set environment variables

--- config-example.py
+++ config-example.py
@@ -1,14 +1,17 @@
-BOT_NAME = "GetTempBot"
-GOOGLE_URL_SHORTEN_API = ""
-API_TOKEN = ""
-WEBHOOK_URL = "https://your-heroku-host.herokuapp.com"
-CHANNEL_REPLY_ID = "-100XXXXXXXXXX"
-CHANNEL_REPLY_TELETHON_ID = XXXXXXXXXX
-CHANNEL_REPLY_TELETHON_HASH = -XXXXXXXXXXXXXXXXXX
-POLLING = False
-DB_URL = 'postgres://root@localhost:5432/GetTempBot'
+import os
 
-CLIENT_API_ID = 
-CLIENT_API_HASH = ""
-CLIENT_PHONE = ""
-CLIENT_2FA_PASSWORD = ""
+BOT_NAME = "HK_TempTelegram"
+GOOGLE_URL_SHORTEN_API = ""
+API_TOKEN = os.environ.get("API_TOKEN")
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+CHANNEL_REPLY_ID = os.environ.get("CHANNEL_REPLY_ID")
+CHANNEL_REPLY_TELETHON_ID = os.environ.get("TELETHON_ID")
+CHANNEL_REPLY_TELETHON_HASH = os.environ.get("TELETHON_HASH")
+POLLING = False
+DB_URL = os.environ.get("DB_URL")
+#DB_URL = 'postgres://root@localhost:5432/GetTempBot'
+
+CLIENT_API_ID = os.environ.get("APP_ID")
+CLIENT_API_HASH =os.environ.get("API_HASH)
+CLIENT_PHONE = os.environ.get("PHONE")
+CLIENT_2FA_PASSWORD = os.environ.get("PASSWORD")
